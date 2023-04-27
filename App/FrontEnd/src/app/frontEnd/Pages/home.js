97151a8623be7bfdd89d5e55f64a6519a144979b
Hiago Peroni <hiagoperonip@gmail.com>
2023-04-27 19:49:47 -0300

    Página de Plantas definida por qnt pronta

--- App/FrontEnd/src/app/frontEnd/Pages/home.js
+++ App/FrontEnd/src/app/frontEnd/Pages/home.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderComp from '../Components/HeaderComp';
 import PlantaComp from '../Components/PlantaComp';
 
 function Home() {
+  const qntFreezer = 31;
+  const [qntPlantas, setQuantPlantas] = useState();
+
+  const calcPlantas = () => {
+    const divisao = (qntFreezer / 15);
+    const arredondado = Math.floor(divisao);
+    if (divisao > arredondado) {
+      return setQuantPlantas(arredondado + 1);
+    }
+    return setQuantPlantas(arredondado);
+  }
+
+  useEffect(() => calcPlantas(), [])
+
   return (
     <main className="homePage">
       <HeaderComp />
       <p>HomePage</p>
       <h1>Meus Produtos</h1>
       <div className="dispProd">
-        <PlantaComp />
-        <PlantaComp />
+      {
+        [...Array(qntPlantas)].map((_, index) => (
+          <PlantaComp key={index} />
+        ))
+      }
       </div>
     </main>
   )
--- App/FrontEnd/src/app/frontEnd/Pages/produto.js
+++ App/FrontEnd/src/app/frontEnd/Pages/produto.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import zonaUm from '../../backEnd/teste/gerarFreezers.js';
-import FreezerComp from '../Components/FreezerComp';
-import HeaderComp from '../Components/HeaderComp.js';
-import imgConfig from '../Imagens/Configs.png'
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import zonaUm from '../../backEnd/teste/gerarFreezers.js';
+// import FreezerComp from '../Components/FreezerComp';
+// import HeaderComp from '../Components/HeaderComp.js';
+// import imgConfig from '../Imagens/Configs.png'
 
-function Produto() {
-  const navigate = useNavigate();
-  const goToConfig = () => {
-    return navigate('/produto/config');
-  }
-  return (
-    <main className="homePage">
-      <HeaderComp />
-      <h1>Painel da Planta 01</h1>
-      <div className="divConfig">
-        <img src={ imgConfig } alt="Configurações" className="imgConfig" onClick={ goToConfig }/>  
-      </div>
-      <div className="dispFreezer">
-        <div className='uEsquerda'>
-          {
-            zonaUm.map((cadaFreezer) => {
-              return cadaFreezer.id < 6 && <FreezerComp 
-                key={ cadaFreezer.id }
-                id={ cadaFreezer.id }
-                temp={ cadaFreezer.temp }
-                porta={ cadaFreezer.porta }
-                />
-            })
-          }
-        </div>
-        <div className='uBaixo'>
-          {
-            zonaUm.map((cadaFreezer) => {
-              return (cadaFreezer.id > 5 && cadaFreezer.id < 11) &&
-              <FreezerComp 
-                key={ cadaFreezer.id }
-                id={ cadaFreezer.id }
-                temp={ cadaFreezer.temp }
-                porta={ cadaFreezer.porta }
-                />
-              }
-            )
-          }
-        </div>
-        <div className='uDireita'>
-          {
-            zonaUm.map((cadaFreezer) => {
-              return (cadaFreezer.id > 10) &&
-              <FreezerComp 
-                key={ cadaFreezer.id }
-                id={ cadaFreezer.id }
-                temp={ cadaFreezer.temp }
-                porta={ cadaFreezer.porta }
-                />
-            })
-          }
-        </div>
-      </div>
-    </main>
-  )
-}
+// function Produto() {
+//   const navigate = useNavigate();
+//   const goToConfig = () => {
+//     return navigate('/produto/config');
+//   }
+//   return (
+//     <main className="homePage">
+//       <HeaderComp />
+//       <h1>Painel da Planta 01</h1>
+//       <div className="divConfig">
+//         <img src={ imgConfig } alt="Configurações" className="imgConfig" onClick={ goToConfig }/>  
+//       </div>
+//       <div className="dispFreezer">
+//         <div className='uEsquerda'>
+//           {
+//             zonaUm.map((cadaFreezer) => {
+//               return cadaFreezer.id < 6 && <FreezerComp 
+//                 key={ cadaFreezer.id }
+//                 id={ cadaFreezer.id }
+//                 temp={ cadaFreezer.temp }
+//                 porta={ cadaFreezer.porta }
+//                 />
+//             })
+//           }
+//         </div>
+//         <div className='uBaixo'>
+//           {
+//             zonaUm.map((cadaFreezer) => {
+//               return (cadaFreezer.id > 5 && cadaFreezer.id < 11) &&
+//               <FreezerComp 
+//                 key={ cadaFreezer.id }
+//                 id={ cadaFreezer.id }
+//                 temp={ cadaFreezer.temp }
+//                 porta={ cadaFreezer.porta }
+//                 />
+//               }
+//             )
+//           }
+//         </div>
+//         <div className='uDireita'>
+//           {
+//             zonaUm.map((cadaFreezer) => {
+//               return (cadaFreezer.id > 10) &&
+//               <FreezerComp 
+//                 key={ cadaFreezer.id }
+//                 id={ cadaFreezer.id }
+//                 temp={ cadaFreezer.temp }
+//                 porta={ cadaFreezer.porta }
+//                 />
+//             })
+//           }
+//         </div>
+//       </div>
+//     </main>
+//   )
+// }
 
-export default Produto;
+// export default Produto;
